2e20a0701f8d6b34b7b2535a0581526577df4a22
mikai <dreamfever2017@yahoo.com>
2026-05-01 17:36:25 +0800

    build: update dependency versions

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.3.20")
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.3.21")
 }
